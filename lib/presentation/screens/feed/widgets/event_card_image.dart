cc3c797fa8bfbdf5b0f15c0418e7169dfb46609b
hashem78 <hashem.olayano@gmail.com>
2021-02-14 02:40:18 +0200

    Force AspectRatio on Event Card

--- lib/presentation/screens/feed/widgets/event_card_image.dart
+++ lib/presentation/screens/feed/widgets/event_card_image.dart
@@ -1,7 +1,5 @@
-import 'dart:math';
-
 import 'package:aussie/models/event/event_model.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
@@ -14,14 +12,21 @@ class EventCardImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final EventModel e = Provider.of<EventModel>(context);
-    return SizedBox(
-      height: min(.5.sh, e.bannerImage.height.toDouble()),
-      width: double.infinity,
-      child: Ink.image(
+    return AspectRatio(
+      aspectRatio: 16 / 9,
+      child: CachedNetworkImage(
+        imageUrl: e.bannerImage.imageLink,
         fit: BoxFit.fill,
-        image: CachedNetworkImageProvider(
-          e.bannerImage.imageLink,
-        ),
+        imageBuilder: (context, imageProvider) {
+          return Ink.image(
+            image: imageProvider,
+          );
+        },
+        progressIndicatorBuilder: (context, url, progress) {
+          return Center(
+            child: CircularProgressIndicator(value: progress.progress),
+          );
+        },
       ),
     );
   }
