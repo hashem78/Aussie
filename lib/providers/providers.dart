13a28b05f572e9b15477ce63520d5a84728fa249
hashem78 <hashem.olayano@gmail.com>
2022-02-19 21:25:19 +0200

    remove more useless junk

--- lib/providers/providers.dart
+++ lib/providers/providers.dart
@@ -1,6 +1,4 @@
 library aussie_providers;
 
 export '../repositories/attendees_repository.dart';
-export 'paginated_searchable_provider.dart';
-export 'thumbnail_provider.dart';
 export '../state/user_management.dart';
