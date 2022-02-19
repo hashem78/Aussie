23d1335aa52f1a41d64e422761f51db345b607be
hashem78 <hashem.olayano@gmail.com>
2022-02-19 15:01:49 +0200

    remove useless widet and state

--- lib/presentation/widgets/aussie/aussie_widgets.dart
+++ lib/presentation/widgets/aussie/aussie_widgets.dart
@@ -3,4 +3,3 @@ library aussie_widgets;
 export 'app_drawer.dart';
 export 'aussie_photo_view.dart';
 export 'scaffold.dart';
-export 'thumbnailed_appbar.dart';
--- lib/state/state.dart
+++ lib/state/state.dart
@@ -7,4 +7,3 @@ export 'form_bloc/form_bloc.dart';
 export 'multi_image_picking_cubit/multi_image_picking_cubit.dart';
 export 'signup_cubit/signup_bloc.dart';
 export 'single_image_picking_cubit/single_image_picking_cubit.dart';
-export 'thumbnail_cubit/thumbnail_cubit.dart';
