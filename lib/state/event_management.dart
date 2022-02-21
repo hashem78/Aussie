126c90d847b5411c818aeadfb9b1172ba68a2a6f
hashem78 <hashem.olayano@gmail.com>
2022-02-21 16:44:49 +0200

    add auto disposed to event provider

--- lib/state/event_management.dart
+++ lib/state/event_management.dart
@@ -1,7 +1,7 @@
 import 'package:aussie/models/event/event_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final scopedEventProvider = Provider<EventModel>(
+final scopedEventProvider = Provider.autoDispose<EventModel>(
   (_) {
     throw UnimplementedError();
   },
